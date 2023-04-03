c16451bcea0422e8788a8ed7f0ee58366b58b4d1
Matheus Ferreira <matheus.fer33@gmail.com>
2023-04-02 21:45:17 -0300

    added the hability to get a single movie by imdb id

--- server/src/main/java/com/matheusfer/moviesAPI/MovieService.java
+++ server/src/main/java/com/matheusfer/moviesAPI/MovieService.java
@@ -1,9 +1,11 @@
 package com.matheusfer.moviesAPI;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,9 +14,12 @@ public class MovieService {
     private MovieRepository movieRepository;
 
     public List<Movie> allMovies() {
-
         return movieRepository.findAll();
+    }
 
+    //a tag optional indica ao java que a função pode retornar nulo (o id passado pode não ser encontrado e o return será null)
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 
 }
