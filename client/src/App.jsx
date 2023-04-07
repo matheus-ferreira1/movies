637731c6cd5639fd431f24b242ba774feada233e
Matheus Ferreira <matheus.fer33@gmail.com>
2023-04-07 18:39:31 -0300

    created front end carousel and initial styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/home/Home";
+import "./App.css";
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+
+      setMovies(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div>
-      <h1>teste</h1>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
